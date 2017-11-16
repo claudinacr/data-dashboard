62c54c54aff07e31761191ee069276120c14e4d8
Claudina Cortez <claudinacr@gmail.com>
2017-11-16 17:16:06 -0300

    Primer cambio

--- js/app.js
+++ js/app.js
@@ -4,3 +4,19 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+/* Total de estudiantes por Sede y Generación */
+
+/* Total de estudiantes por Sede */
+
+/* Arequipa */
+var totalAQP = Object.keys(data.AQP);
+/* Cantidad de generaciones en Arequipa */
+
+/* Cantidad de estudiantes por Generacion en Arequipa*/
+
+
+
+/* Cantidad de estudiantes por Generacion en Arequipa */
+
+
