12c48381e83fa1c9b3e7ac0cf4eade931621affa
ValeOlivares <valentinaolivares85@gmail.com>
2017-11-17 18:21:41 -0300

    subiendo botones

--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
-/*
- * Funcionalidad de tu producto
- */
-
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
-
-/* Total de estudiantes por Sede y Generación */
-
-/* Total de estudiantes por Sede */
-
-/* Arequipa */
-var totalAQP = Object.keys(data.AQP);
-/* Cantidad de generaciones en Arequipa */
-
-/* Cantidad de estudiantes por Generacion en Arequipa*/
-
-
-
-/* Cantidad de estudiantes por Generacion en Arequipa */
-
-
+//cambiar contenido segun boton del menu
+var mostrarOcultar = function(e){
+  var tabSeleccionado = e.target.dataset.tabSeleccionado;
+  var overview = document.getElementById("overview");
+  var student = document.getElementById("student");
+  var teacher = document.getElementById("teacher");
+  if (tabSeleccionado === "tabOverview"){
+    console.log ("overview");
+    student.style.display="none"; //oculta pantalla student
+    teacher.style.display="none"; //oculta pantalla teacher
+    overview.style.display="block"; //muestra pantalla overview
+  } else if (tabSeleccionado === "tabStudent"){
+    console.log("Student");
+    student.style.display="block"; //muestra pantalla student
+    teacher.style.display="none"; //oculta pantalla teacher
+    overview.style.display="none"; //oculta pantalla overview
+  }else if (tabSeleccionado === "tabTeacher"){
+    console.log("Teacher");
+    student.style.display="none"; //oculta pantalla student
+    teacher.style.display="block"; //muestra pantalla teacher
+    overview.style.display="none"; //oculta pantalla overview
+  }
+}
+var cargarPag = function(){
+  var elementosTab = document.getElementsByClassName("tab");
+  for (var i = 0; i < elementosTab.length; i++) {
+    elementosTab[i].addEventListener("click" , mostrarOcultar);
+  }
+}
+cargarPag();
